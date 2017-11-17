7069f205b1daeb75ee5dde8bb047ca5fb1c13645
DaniaLD <danial.6274@gmail.com>
2017-11-17 23:14:32 +0330

    Setting up template engine

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Setting view engine
+app.set('view engine', 'ejs');
+
+
+// Setting public for static files
+app.use('/', express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
-  res.send("Hello world!");
+  res.render("home");
 });
 
 // Setting up port
