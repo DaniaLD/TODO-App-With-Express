2e26dc64b2598e4999e9f2da9dbfad05494a806b
DaniaLD <danial.6274@gmail.com>
2017-11-19 19:37:33 +0330

    Reading and Saving tasks, Quey, Showing them

--- routs/index.js
+++ routs/index.js
@@ -4,7 +4,14 @@ const Task        = require('../models/task');
 
 // Home
 let home =  (req, res) => {
-    res.render('home');
+    Task.distinct('taskName', (err,data) => {
+        if(err) {
+            console.log("Can not read data from database !!! -> " + err)
+        } else {
+            console.log("Read data from database successfully ...");
+            res.render('home', { tasks: data });
+        }
+    });
 };
 
 // 404
@@ -17,9 +24,14 @@ let sendData = (req, res) => {
     let task = new Task();
     task.taskName = req.body.value;
 
-    task.save();
+    task.save( (err) => {
+        if(err) {
+            console.log("Item didn't save !!! -> " + err);
+        } else {
+            console.log("Item saved successfully ... -> " + task)
+        }
+    });
 
-    console.log(task);
     res.redirect('/');
 };
 
